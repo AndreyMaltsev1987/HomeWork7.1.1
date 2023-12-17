3abe23b5bd1010aa00bac173763fc08ab449f231
Andrey <andryuxa16@yandex.ru>
2023-12-17 22:04:55 +0300

    add commit Task03.java

---
+++ src/Task03.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+
+public class Task03 {
+    public static void main(String[] args) {
+        System.out.println("Задание 3");
+        Scanner scanner = new Scanner(System.in);
+    }
+}
+
